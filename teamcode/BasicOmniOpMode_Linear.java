1c90aaba527a2674cca525d98bcf8bf9052a48b6
LRGuess <95448250+LRGuess@users.noreply.github.com>
2022-11-22 19:49:13 -0700

    Added the very first version of our code-
    AIM ASSIST NOT COMPLETE THIS IS BASIC DRIVE 
    ARM CODE NOT TESTED

---
+++ teamcode/BasicOmniOpMode_Linear.java
@@ -0,0 +1,164 @@
+/* Copyright (c) 2021 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name="Basic: Omni Linear OpMode", group="Linear Opmode")
+@Disabled
+public class BasicOmniOpMode_Linear extends LinearOpMode {
+
+    // Declare OpMode members for each of the 4 motors.
+
+
+    @Override
+    public void runOpMode() {
+
+        double armMotorSpeed = 1.0;
+        double wheelsMotorSpeed = 1.0;
+        double aimAssistMotorSpeed = 0.1;
+
+        boolean useAimAssist = true;
+
+        DcMotor greenMotor;
+        DcMotor blackMotor;
+        DcMotor armMotor;
+
+        Servo rightHandServo;
+        Servo leftHandServo;
+
+        DigitalChannel centerTouchSensor;
+
+        greenMotor = hardwareMap.get (DcMotor.class, "greenMotor");
+        blackMotor = hardwareMap.get (DcMotor.class, "blackMotor");
+        armMotor = hardwareMap.get (DcMotor.class, "armMotor");
+        rightHandServo = hardwareMap.get (Servo.class, "rightHandServo");
+        leftHandServo = hardwareMap.get (Servo.class, "leftHandServo");
+        centerTouchSensor = hardwareMap.get (DigitalChannel.class, "centerTouchSensor");
+
+        greenMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        blackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        centerTouchSensor.setMode(DigitalChannel.Mode.INPUT);
+
+        //region telemetry
+        // Wait for the game to start (driver presses PLAY)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        //endregion
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            /**===================================================================================/*
+            -------------------------------=AIM ASSIST INFO=--------------------------------------/*
+            ======================================================================================/*
+
+            Aim Assist is to help you put the cone on the pole without dropping it and with
+            maximum precision. This is done by reading the input from some sensors such as
+            the touch sensor. By reading the info it will regulate the motor speeds and half
+            drive the robot to help you if you do not interfere.
+
+            =======INTERFERING WITH THE AIM ASSIST WILL CAUSE YOU TO LOSE MOTOR POWER AND YOU
+                   WILL PROBABLY LOSE THE CONE AND SOME CONTROL FOR THE ROBOT================
+             */
+            //Drive code
+            greenMotor.setPower(gamepad1.left_stick_y);
+            blackMotor.setPower(gamepad1.right_stick_y);
+
+            //Increase the motor speed for the arm by pressing the left bumper gamepad2
+            if (gamepad2.left_bumper)
+                armMotorSpeed = armMotorSpeed + 0.1;
+                //If you try to make the motor speed higher then 1.0, set the speed to 1.0.
+                if (armMotorSpeed >= 1.0)
+                    armMotorSpeed = 1.0;
+
+            //Decrease the motor speed for the arm by pressing the right bumper on gamepad2
+            if (gamepad2.right_bumper)
+                armMotorSpeed = armMotorSpeed - 0.1;
+                //If you try to make the motor speed lower then 0, set the speed to 0.0.
+                if (armMotorSpeed <= 0.0)
+                    armMotorSpeed = 0.0;
+
+            //Increase the motor speed for the wheels by pressing the left bumper on gamepad1
+            if (gamepad1.left_bumper)
+                wheelsMotorSpeed = wheelsMotorSpeed + 0.1;
+                //If you try to make the motor speed higher then 1.0, set the speed to 1.0.
+                if (wheelsMotorSpeed >= 1.0)
+                    wheelsMotorSpeed = 1.0;
+
+            //Decrease the motor speed for the wheels by pressing the right bumper on gamepad1
+            if (gamepad1.right_bumper)
+                wheelsMotorSpeed = wheelsMotorSpeed - 0.1;
+                if (wheelsMotorSpeed <= 0.0)
+                    //If you try to make the motor speed lower then 0, set the speed to 0.0.
+                    wheelsMotorSpeed = 0.0;
+
+            //Raise the arm when you press the a button on gamepad2
+            if (gamepad2.a)
+                armMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                armMotor.setPower(armMotorSpeed);
+
+            //Lower the arm when you press the b button on gamepad2
+            if (gamepad2.b)
+                armMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+                armMotor.setPower(armMotorSpeed);
+
+            //Toggle AimAssist when you press the x button on gamepad1
+            if (gamepad1.x)
+                if (useAimAssist)
+                    useAimAssist = false;
+                if (!useAimAssist)
+                    useAimAssist = true;
+
+            //AimAssist code
+            if (useAimAssist)
+                if (centerTouchSensor.getState())
+                    //not pressed
+                    return;
+                else if (!centerTouchSensor.getState())
+                    //pressed
+                    aimAssistMotorSpeed = wheelsMotorSpeed / 3;
+                    greenMotor.setPower(aimAssistMotorSpeed);
+                    greenMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+                    blackMotor.setPower(aimAssistMotorSpeed);
+                    blackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.update();
+        }
+    }}
