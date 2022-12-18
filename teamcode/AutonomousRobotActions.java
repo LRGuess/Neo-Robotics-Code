674f7e68c148e08c5d7de19a49ab66f656939995
Liam <95448250+LRGuess@users.noreply.github.com>
2022-12-18 12:55:14 -0700

    Update AutonomousRobotActions.java

---
+++ teamcode/AutonomousRobotActions.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class AutonomousRobotActions {
+    public void moveForward(int duration, DcMotor _greenMotor, DcMotor _blackMotor) {
+        _greenMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        _blackMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        _greenMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        _blackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        _greenMotor.setTargetPosition(duration);
+        _blackMotor.setTargetPosition(duration);
+
+        _greenMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        _blackMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public void moveBackwards(int duration, DcMotor _greenMotor, DcMotor _blackMotor){
+        _greenMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        _blackMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        _greenMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        _blackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        _greenMotor.setTargetPosition(duration);
+        _blackMotor.setTargetPosition(duration);
+
+        _greenMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        _blackMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    }
+
+    public void turnLeft(int duration, DcMotor _greenMotor, DcMotor _blackMotor){
+        _greenMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        _blackMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        _greenMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        _blackMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        _greenMotor.setTargetPosition(duration);
+        _blackMotor.setTargetPosition(duration);
+
+        _greenMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        _blackMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    }
+
+    public void turnRight(int duration, DcMotor _greenMotor, DcMotor _blackMotor){
+        _greenMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        _blackMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        _greenMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        _blackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        _greenMotor.setTargetPosition(duration);
+        _blackMotor.setTargetPosition(duration);
+
+        _greenMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        _blackMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    }
+
+    public void armUp(double power, int duration, DcMotor _armMotor){
+        _armMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        _armMotor.setPower(power);
+
+        _armMotor.setTargetPosition(duration);
+
+        _armMotor.setPower(0);
+
+    }
+
+    public void armDown(double power, int duration, DcMotor _armMotor) throws InterruptedException {
+        _armMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        _armMotor.setPower(power);
+
+        _armMotor.setTargetPosition(duration);
+
+        _armMotor.setPower(0);
+
+    }
+
+    public void closeHand(Servo _rightHandServo, Servo _leftHandServo) throws InterruptedException{
+
+    }
+
+
+
+    public void stop(DcMotor _greenMotor, DcMotor _blackMotor) {
+        _greenMotor.setPower(0);
+        _blackMotor.setPower(0);
+
+    }
+
+    public void stopAll(DcMotor _greenMotor, DcMotor _blackMotor, DcMotor _armMotor, Servo _leftHandServo, Servo _rightHandServo){
+        _greenMotor.setPower(0);
+        _blackMotor.setPower(0);
+        _armMotor.setPower(0);
+
+    }
+}
